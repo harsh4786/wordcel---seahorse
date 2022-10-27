924f987f4364f9d43fdfb7e4b9f221f97b335989
harsh4786 <hp80738@gmail.com>
2022-10-27 18:41:21 +0530

    updates

--- programs_py/wordcel.py
+++ programs_py/wordcel.py
@@ -5,6 +5,7 @@
 from seahorse.prelude import *
 declare_id('Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS')
 
+MAX_LEN_URI: u64 = 128 
 class Profile(Account):
     authority: Pubkey
     bump: u8
@@ -57,9 +58,19 @@ def create_post(
  post_account.random_hash = random_hash
  post_account.bump = post.bump()
 
-#@instruction
-#def update_post(
-  #  user: Signer,
-   # metadata_uri: str, 
-  #  profile: Profile, 
-#):
+@instruction
+def update_post(
+    user: Signer,
+    metadata_uri: str, 
+    profile: Profile, 
+    post: Post,
+):
+# since we have used strings in random_hash we have to do exhaustive checks
+# this will change when seahorse supports array deserialization
+ assert ((post.profile == profile.key()) 
+    and (profile.random_hash != post.random_hash) 
+    and (profile.random_hash != metadata_uri)
+    and (post.random_hash != metadata_uri)
+    and (post.metadata_uri != metadata_uri)), "Invalid parameters"
+
+ 
